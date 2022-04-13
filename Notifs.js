bddd199af269995abdd5d4c6cc03937b4c722114
MarkyMooCow <95723859+MarkyMooCow@users.noreply.github.com>
2022-04-13 15:23:23 -0230

    Notifssssssssss
    Add this to your SRC under the name Notifs.js

---
+++ Notifs.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+export default function Notifs() {
+  return (
+    <div>
+      <h2>Notifs</h2>
+      <p> This will be our notifs component!</p>
+    </div>
+  );
+}
