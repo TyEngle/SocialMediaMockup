671593270bf2bfa5926907547e178889e2284779
MarkyMooCow <95723859+MarkyMooCow@users.noreply.github.com>
2022-04-13 15:19:20 -0230

    Give me a sec gotta get components

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import Home from './Home';
+import Notifs from './Notifs';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
 
 function App() {
   return (
+    <Router>
     <div>
-      <p>Another test code, yay!</p>
+      <h1>Broooooo</h1>
+      <hr />
     </div>
+      <Link to='/'>Home</Link>
+      <Link to='/Notifs'>Notifs</Link>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Notifs" element={<Notifs />} />
+      </Routes>
+    </Router>
   );
 }
 
