3db2a9a2b346942595b24b0f08e44565052096a3
MarkyMooCow <95723859+MarkyMooCow@users.noreply.github.com>
2022-04-13 15:21:26 -0230

    HOMEEEEEEEEEEE
    Add this file with name to da code.

---
+++ Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Home Component</h2>
+      <p> This is my home compoenent - for application</p>
+    </div>
+  );
+}
