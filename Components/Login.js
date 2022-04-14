0007b483a539b108db81cd66b54c0a95d4951018
RileyJohn39 <95235543+RileyJohn39@users.noreply.github.com>
2022-04-14 20:36:26 -0230

    Update Login.js

--- Components/Login.js
+++ Components/Login.js
@@ -3,7 +3,12 @@ import React from 'react'
 export default function Login() {
   return (
     <div>
-        <h1>Hello from Login component</h1>
+    <h1> Login </h1>
+        <h4>Username</h4>
+        <input type="text" id="lname" name="lname"></input>
+        <h4>Password</h4>
+        <input type="text" id="lname" name="lname"></input>
+        <button>Forgot password?</button>    
     </div>
   )
 }
