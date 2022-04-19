2ec6ca79b406432a85c119e5cd3af9dcab509b2b
RileyJohn39 <95235543+RileyJohn39@users.noreply.github.com>
2022-04-19 19:30:53 -0230

    Update Message.js

--- Components/Message.js
+++ Components/Message.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 export default function Message() {
   return (
-    <div>
-        <h1>Hello from Message component</h1>
+    <div id="myHead2">
+        <h1>Messages</h1>
+        <p id="Text1">Hello</p>
+        <p id="Text2">My parents left me.</p>
+        <p id="Text3">Hows it goin</p>
     </div>
   )
 }
