74ac273b98ed8180ce7c7707ac533e93608e0f73
TyEngle <95366088+TyEngle@users.noreply.github.com>
2022-04-20 14:09:26 -0230

    Update Login.js

--- Components/Login.js
+++ Components/Login.js
@@ -1,14 +1,57 @@
 import React from 'react'
+import "./Login.css";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
-export default function Login() {
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const updateUsername = (e) => {
+    setUsername(e.target.value);
+    console.log(username);
+  };
+
+  let navigate = useNavigate();
+  const routeChange = () => {
+    let path = `../home`;
+    navigate(path);
+  };
+
   return (
-    <div>
-    <h1> Login </h1>
-        <h4>Username</h4>
-        <input type="text" id="lname" name="lname"></input>
-        <h4>Password</h4>
-        <input type="text" id="lname" name="lname"></input>
-        <button>Forgot password?</button>    
+    <div className="SignInBox">
+      <form className="SignInForm">
+        <h1>Sign In!</h1>
+        <label for="username">Username:</label>
+        <input
+          name="username"
+          onChange={updateUsername}
+          type="text"
+          placeholder="username"
+          value={username}
+        ></input>
+        <label for="password">Password:</label>
+        <input
+          name="password"
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          placeholder="password"
+          value={password}
+        ></input>
+        <button
+          type="button"
+          onClick={() => {
+            setUsername("");
+            setPassword("");
+          }}
+          onClick={routeChange}
+        >
+          Submit
+        </button>
+      </form>
     </div>
-  )
+  );
 }
+
+export default Login;
+
